e2126cbbbd10380bb9f9064dffd716c54e786185
Sergo Lemon <srg.lemn@gmail.com>
2023-03-17 15:09:22 +0200

    Refactoring.

--- MatrixGame/Program.cs
+++ MatrixGame/Program.cs
@@ -1,18 +1,20 @@
 ﻿internal class Program
 {
+    private const int columnsCount = 5;
+    private const int rowsCount = 5;
+
+    private static readonly PointState[,] _visualTable = new PointState[columnsCount, rowsCount];
+
     private static void Main(string[] args)
     {
-        const int columnsCount = 5;
-        const int rowsCount = 5;
-
         var random = new Random();
-        var targetPosition = (columnNumber: random.Next(1, columnsCount), rowNumber: random.Next(1, rowsCount));
-        var visualTable = new PointState[columnsCount, rowsCount];
+        var targetPosition = new { columnNumber = random.Next(1, columnsCount), rowNumber = random.Next(1, rowsCount) };
 
         while (true)
         {
             Console.WriteLine("All set. Get ready to rumble.");
             RenderTable();
+
             Console.Write("Please enter a column number: ");
             int columnNumber;
             TryEnterNumber(out columnNumber, 1, columnsCount);
@@ -26,60 +28,60 @@
             if (columnNumber == targetPosition.columnNumber && rowNumber == targetPosition.rowNumber)
             {
                 Console.Clear();
-                visualTable[rowIndex, columnIndex] = PointState.IsTarget;
+                _visualTable[rowIndex, columnIndex] = PointState.IsTarget;
                 RenderTable();
-                Console.WriteLine("You WIN!");
+                Console.WriteLine("YOU WIN!");
                 break;
             }
             else
             {
-                visualTable[rowIndex, columnIndex] = PointState.IsEmpty;
+                _visualTable[rowIndex, columnIndex] = PointState.IsEmpty;
                 Console.Clear();
-            }
-        }
-
-        void RenderTable()
-        {
-            for (var i = 0; i <= columnsCount; i++)
-            {
-                Console.Write(i + (i < columnsCount ? " " : ""));
-            }
-
-            Console.WriteLine();
-
-            for (var i = 0; i < rowsCount; i++)
-            {
-                Console.Write($"{i + 1} ");
-
-                for (var j = 0; j < columnsCount; j++)
-                {
-                    Console.Write(GetMarkPoint(visualTable![i, j]).ToString());
-                    Console.Write(j < columnsCount ? " " : "");
-                }
-
-                Console.WriteLine();
             }
         }
     }
 
-    static void TryEnterNumber(out int inputBuffer, int minNumber, int maxNumber)
+    private static void RenderTable()
+    {
+        for (var i = 0; i <= columnsCount; i++)
+        {
+            Console.Write(i + (i < columnsCount ? " " : ""));
+        }
+
+        Console.WriteLine();
+
+        for (var i = 0; i < rowsCount; i++)
+        {
+            Console.Write($"{i + 1} ");
+
+            for (var j = 0; j < columnsCount; j++)
+            {
+                Console.Write(GetMarkPoint(_visualTable![i, j]).ToString());
+                Console.Write(j < columnsCount ? " " : "");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    private static void TryEnterNumber(out int inputBuffer, int minNumber, int maxNumber)
     {
         if (!int.TryParse(Console.ReadLine(), out inputBuffer) ||
             inputBuffer < minNumber ||
             inputBuffer > maxNumber)
         {
-            Console.Write("Invalid input, please try again: ");
+            Console.Write("Invalid number input, please try again: ");
             TryEnterNumber(out inputBuffer, minNumber, maxNumber);
         }
     }
 
 
-    static void TryEnterNumber(out int inputBuffer, int maxNumber)
+    private static void TryEnterNumber(out int inputBuffer, int maxNumber)
     {
         TryEnterNumber(out inputBuffer, 0, maxNumber);
     }
 
-    static char GetMarkPoint(PointState pointState)
+    private static char GetMarkPoint(PointState pointState)
     {
         switch (pointState)
         {
